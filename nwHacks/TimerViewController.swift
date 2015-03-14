c1bc89615d7fab97f88e39dc8bf4798e8220ad41
Andrew Richardson <andrewr114@gmail.com>
2015-03-14 05:20:42 -0700

    Added timer view controller and circular progress view dependency (via CocoaPods)

---
+++ nwHacks/TimerViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TimerViewController.swift
+//  nwHacks
+//
+//  Created by Andrew Richardson on 3/14/15.
+//  Copyright (c) 2015 Andrew Richardson. All rights reserved.
+//
+
+import UIKit
+
+private let formatter: NSDateFormatter = {
+    let formatter = NSDateFormatter()
+    return formatter
+}()
+
+class TimerViewController: UIViewController {
+    @IBOutlet var timeLabel: UILabel!
+    var fireDate = NSDate(timeIntervalSinceNow: 15)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func updateTime() {
+        let timeRemaining = fireDate.timeIntervalSinceNow
+//        timeLabel.text =
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
