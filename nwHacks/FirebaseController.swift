b48c3fb4ed6b926564e6f58bf706460d9ebbdd69
Patrick Wilkie <plwilkie@gmail.com>
2015-03-14 16:05:29 -0700

    Added Firebase Controller

---
+++ nwHacks/FirebaseController.swift
@@ -0,0 +1,11 @@
+//
+//  FirebaseController.swift
+//  nwHacks
+//
+//  Created by Patrick Wilkie on 2015-03-14.
+//  Copyright (c) 2015 Andrew Richardson. All rights reserved.
+//
+
+import Foundation
+
+
